8a3142fc9da72303ed933604c2d378176072a80f
Adam Skonieczny <askonieczny@umass.edu>
2017-06-07 14:47:57 -0500

    Handling successful user submission redirects

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       # Handle a successful save.
+      redirect_to user_url(@user)
     else
       render 'new'
     end
