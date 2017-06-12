3061f1367df0156e41dd588b7391220f0ec2dd10
Adam Skonieczny <askonieczny@umass.edu>
2017-06-12 09:22:02 -0500

    Layout changes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,14 +4,15 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
-      # Log the user in and redirect to the user's show page.
-    else
-      # Create an error message.
-      render 'new'
+      user = User.find_by(email: params[:session][:email].downcase)
+      if user && user.authenticate(params[:session][:password])
+        log_in user
+        redirect_to user
+      else
+        flash.now[:danger] = 'Invalid email/password combination'
+        render 'new'
+      end
     end
-  end
 
   def destroy
   end
