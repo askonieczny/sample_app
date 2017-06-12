642f9edd0f61dd28d4d8bacf52628e856663bce8
Adam Skonieczny <askonieczny@umass.edu>
2017-06-12 15:48:36 -0500

    Prepping for account activation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
-  before_save { self.email = email.downcase }
+  attr_accessor :remember_token, :activation_token
+  before_save   :downcase_email
+  before_create :create_activation_digest
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\b[A-Z0-9._%a-z\-]+@(?:[A-Z0-9a-z\-]+\.)+[A-Za-z]{2,4}\z/
   validates :email, presence: true, length: { maximum: 255 },
@@ -38,4 +39,16 @@ end
     update_attribute(:remember_digest, nil)
   end
 
+  private
+
+    # Converts email to all lower-case.
+    def downcase_email
+      email.downcase!
+    end
+
+    # Creates and assigns the activation token and digest.
+    def create_activation_digest
+      self.activation_token  = User.new_token
+      self.activation_digest = User.digest(activation_token)
+    end
 end
