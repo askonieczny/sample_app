42b4585a929b91cea5d5d50977891d97aa3ff65e
Adam Skonieczny <askonieczny@umass.edu>
2017-06-13 15:56:19 -0500

    Finished adding user microposts

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -40,6 +40,15 @@ class PasswordResetsController < ApplicationController
 
   private
 
+  def micropost_params
+      params.require(:micropost).permit(:content, :picture)
+    end
+
+    def correct_user
+      @micropost = current_user.microposts.find_by(id: params[:id])
+      redirect_to root_url if @micropost.nil?
+    end
+
     def user_params
       params.require(:user).permit(:password, :password_confirmation)
     end
